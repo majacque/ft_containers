429d8bcd9415c9a9fd82235092bc881ab2af553d
Matthieu Jacquet <majacque@student.42.fr>
2022-08-17 07:50:41 +0200

    [ALGORITHM] [UPDATE] add description on lexicographical_compare	[OK]

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -57,8 +57,20 @@ bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, B
 	return true;
 }
 
-// TODO lexicographical_compare
-
+/**
+ * @brief Returns true if the range [first1,last1) compares lexicographically less than the range [first2,last2)
+ * 
+ * @par A lexicographical comparison is the kind of comparison generally used to sort words alphabetically in dictionaries;
+ * It involves comparing sequentially the elements that have the same position in both ranges against each other until one element is not equivalent to the other.
+ * The result of comparing these first non-matching elements is the result of the lexicographical comparison.
+ * If both sequences compare equal until one of them ends, the shorter sequence is lexicographically less than the longer one.
+ * The elements are compared using operator<.
+ * The objects in the ranges [first1,last1) and [first2,last2) are accessed.
+ * 
+ * @param first1 @param last1 Input iterators to the initial and final positions of the first sequence.
+ * The range used is [first1,last1), which contains all the elements between first1 and last1, including the element pointed by first1 but not the element pointed by last1.
+ * @param first2 @param last2 Input iterators to the initial and final positions of the second sequence. The range used is [first2,last2).
+ */
 template <class InputIterator1, class InputIterator2>
 bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
 {
@@ -74,6 +86,24 @@ bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputI
 	return first2 != last2;
 }
 
+/**
+ * @brief Returns true if the range [first1,last1) compares lexicographically less than the range [first2,last2)
+ * 
+ * @par A lexicographical comparison is the kind of comparison generally used to sort words alphabetically in dictionaries;
+ * It involves comparing sequentially the elements that have the same position in both ranges against each other until one element is not equivalent to the other.
+ * The result of comparing these first non-matching elements is the result of the lexicographical comparison.
+ * If both sequences compare equal until one of them ends, the shorter sequence is lexicographically less than the longer one.
+ * The elements are compared using @a comp.
+ * The objects in the ranges [first1,last1) and [first2,last2) are accessed.
+ * 
+ * @param first1 @param last1 Input iterators to the initial and final positions of the first sequence.
+ * The range used is [first1,last1), which contains all the elements between first1 and last1, including the element pointed by first1 but not the element pointed by last1.
+ * @param first2 @param last2 Input iterators to the initial and final positions of the second sequence. The range used is [first2,last2).
+ * @param comp Binary function that accepts two arguments of the types pointed by the iterators, and returns a value convertible to bool.
+ * The value returned indicates whether the first argument is considered to go before the second in the specific strict weak ordering it defines.
+ * The function shall not modify any of its arguments.
+ * This can either be a function pointer or a function object.
+ */
 template <class InputIterator1, class InputIterator2, class Compare>
 bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2,
 							Compare comp)
