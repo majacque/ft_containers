ac6bc73054aa8b5fa166557259471c8c6085c4bc
Matthieu Jacquet <majacque@student.42.fr>
2022-08-17 10:28:26 +0200

    [UTILITY] [ADD] pair operator=                  [OK] pair relational operators       [OK] make_pair                       [TODO]

--- include/utility.hpp
+++ include/utility.hpp
@@ -1,6 +1,9 @@
 #ifndef UTILITY_HPP
 # define UTILITY_HPP
 
+// DBG
+#include <iostream>
+
 namespace ft
 {
 
@@ -14,6 +17,10 @@ struct pair
 
 	T1	first;
 	T2	second;
+
+	/**************************************************************************/
+	/*                               CONSTRUCTOR                              */
+	/**************************************************************************/
 
 	/**
 	 * @brief Constructs a pair object with its elements value-initialized
@@ -29,7 +36,7 @@ struct pair
 	 * @param a An object of the type of first, or some other type implicitly convertible to it
 	 * @param b An object of the type of second, or some other type implicitly convertible to it
 	 */
-	pair (const first_type& a, const second_type& b): first(a), second(b)
+	pair( const first_type& a, const second_type& b ): first(a), second(b)
 	{
 		return;
 	}
@@ -43,11 +50,96 @@ struct pair
 	 * or of a pair type whose element's types are implicitly convertible to those in the pair being constructed.
 	 */
 	template<class U, class V>
-	pair (pair<U,V> const &pr): first(pr.first), second(pr.second)
+	pair( pair<U,V> const &pr ): first(pr.first), second(pr.second)
 	{
 		return;
 	}
+
+	/**************************************************************************/
+	/*                               DESTRUCTOR                               */
+	/**************************************************************************/
+
+	/**
+	 * @brief Destroy the pair object
+	 */
+	~pair( void )
+	{
+		return;
+	}
+
+	/**************************************************************************/
+	/*                            MEMBER FUNCTIONS                            */
+	/**************************************************************************/
+
+	/**************************************************************************/
+	/*                            MEMBER OPERATORS                            */
+	/**************************************************************************/
+
+	/**
+	 * @brief Assigns @a pr as the new content for the pair object.
+	 * 
+	 * @par Member first is assigned pr.first, and member second is assigned pr.second
+	 * 
+	 * @param pr Another pair object.
+	 * This may be an object of the same type or of a pair type whose elements' types are implicitly convertible.
+	 * @return A reference on the instance
+	 */
+	pair& operator=( const pair& pr )
+	{
+		first = pr.first;
+		second = pr.second;
+		return *this;
+	}
 };
+
+/**
+ * @brief Performs the appropriate comparison operation between the pair objects lhs and rhs
+ * 
+ * @par Two pair objects compare equal to each other if both their first members compare equal to each other
+ * and both their second members compare also equal to each other (in both cases using operator== for the comparison).
+ * Similarly, operators <, >, <= and >= perform a lexicographical comparison on the sequence formed by members first and second
+ * (in all cases using operator< reflexively for the comparisons).
+ * 
+ * @param lhs A pair object
+ * @param rhs A pair object
+ * @return true if the condition holds, and false otherwise
+ */
+
+template <class T1, class T2>
+bool operator==( pair<T1,T2> const &lhs, pair<T1,T2> const &rhs )
+{
+	return lhs.first == rhs.first && lhs.second == rhs.second;
+}
+
+template <class T1, class T2>
+bool operator!=( pair<T1,T2> const &lhs, pair<T1,T2> const &rhs )
+{
+	return !(lhs == rhs);
+}
+
+template <class T1, class T2>
+bool operator<( pair<T1,T2> const &lhs, pair<T1,T2> const &rhs )
+{
+	return lhs.first < rhs.first ||(!(rhs.first < lhs.first) && lhs.second < rhs.second);
+}
+
+template <class T1, class T2>
+bool operator<=( pair<T1,T2> const &lhs, pair<T1,T2> const &rhs )
+{
+	return !(rhs < lhs);
+}
+
+template <class T1, class T2>
+bool operator>( pair<T1,T2> const &lhs, pair<T1,T2> const &rhs )
+{
+	return rhs < lhs;
+}
+
+template <class T1, class T2>
+bool operator>=( pair<T1,T2> const &lhs, pair<T1,T2> const &rhs )
+{
+	return !(lhs < rhs);
+}
 
 // TODO make_pair
 
--- src/test_utility.cpp
+++ src/test_utility.cpp
@@ -8,18 +8,23 @@
 #include <utility>
 #include <iostream>
 
+static int				nb = 42;
+static char				c = '*';
+static std::string		str("Artus");
+static std::vector<int>	vec(6, 7);
+
 inline static bool	__pair_default_constructor( void )
 {
 	std::string									s;
 	std::vector<int>							v;
-	ft::pair< std::string, std::vector<int> >	p;
+	ft::pair< std::string, std::vector<int> >	pr;
 
 	if (typeid(ft::pair< std::string, std::vector<int> >::first_type) != typeid(s) ||
 		typeid(ft::pair< std::string, std::vector<int> >::second_type) != typeid(v))
 		return false;
-	else if(typeid(p.first) != typeid(s) || typeid(p.second) != typeid(v))
+	else if(typeid(pr.first) != typeid(s) || typeid(pr.second) != typeid(v))
 		return false;
-	else if (p.first != s || p.second != v)
+	else if (pr.first != s || pr.second != v)
 		return false;
 
 	return true;
@@ -27,28 +32,24 @@ inline static bool	__pair_default_constructor( void )
 
 inline static bool	__pair_initialization_constructor( void )
 {
-	int		i = 42;
-	char	c = '*';
-	ft::pair<int, char>	p0(i, c);
+	ft::pair<int, char>	pr0(nb, c);
 
-	if (typeid(ft::pair<int, char>::first_type) != typeid(i) ||
+	if (typeid(ft::pair<int, char>::first_type) != typeid(nb) ||
 		typeid(ft::pair<int, char>::second_type) != typeid(c))
 		return false;
-	else if(typeid(p0.first) != typeid(i) || typeid(p0.second) != typeid(c))
+	else if(typeid(pr0.first) != typeid(nb) || typeid(pr0.second) != typeid(c))
 		return false;
-	else if (p0.first != i || p0.second != c)
+	else if (pr0.first != nb || pr0.second != c)
 		return false;
 
-	std::string									s("Artus");
-	std::vector<int>							v(6, 7);
-	ft::pair< std::string, std::vector<int> >	p1(s, v);
+	ft::pair< std::string, std::vector<int> >	pr1(str, vec);
 
-	if (typeid(ft::pair< std::string, std::vector<int> >::first_type) != typeid(s) ||
-		typeid(ft::pair< std::string, std::vector<int> >::second_type) != typeid(v))
+	if (typeid(ft::pair< std::string, std::vector<int> >::first_type) != typeid(str) ||
+		typeid(ft::pair< std::string, std::vector<int> >::second_type) != typeid(vec))
 		return false;
-	else if(typeid(p1.first) != typeid(s) || typeid(p1.second) != typeid(v))
+	else if(typeid(pr1.first) != typeid(str) || typeid(pr1.second) != typeid(vec))
 		return false;
-	else if (p1.first != s || p1.second != v)
+	else if (pr1.first != str || pr1.second != vec)
 		return false;
 
 	return true;
@@ -56,19 +57,15 @@ inline static bool	__pair_initialization_constructor( void )
 
 inline static bool	__pair_copy_constructor( void )
 {
-	int		i = 42;
-	char	c = '*';
-	ft::pair<int, char>	p0(i, c);
-	ft::pair<int, char>	p1(p0);
+	ft::pair<int, char>	pr0(nb, c);
+	ft::pair<int, char>	pr1(pr0);
 
-	if(typeid(p0.first) != typeid(p1.first) || typeid(p0.second) != typeid(p1.second))
+	if(typeid(pr0.first) != typeid(pr1.first) || typeid(pr0.second) != typeid(pr1.second))
 		return false;
-	else if (p0.first != p1.first || p0.second != p1.second)
+	else if (pr0.first != pr1.first || pr0.second != pr1.second)
 		return false;
 
-	std::string									s("Artus");
-	std::vector<int>							v(6, 7);
-	ft::pair< std::string, std::vector<int> >	p2(s, v);
+	ft::pair< std::string, std::vector<int> >	p2(str, vec);
 	ft::pair< std::string, std::vector<int> >	p3(p2);
 
 	if(typeid(p2.first) != typeid(p3.first) || typeid(p2.second) != typeid(p3.second))
@@ -79,15 +76,85 @@ inline static bool	__pair_copy_constructor( void )
 	return true;
 }
 
+inline static bool	__pair_assignment_operator( void )
+{
+	ft::pair<int,char>	pr0(nb,c);
+	ft::pair<int,char>	pr1;
+
+	pr1 = pr0;
+
+	if(typeid(pr0.first) != typeid(pr1.first) || typeid(pr0.second) != typeid(pr1.second))
+		return false;
+	else if (pr0.first != pr1.first || pr0.second != pr1.second)
+		return false;
+
+	ft::pair< std::string, std::vector<int> >	p2(str, vec);
+	ft::pair< std::string, std::vector<int> >	p3;
+
+	p3 = p2;
+
+	if(typeid(p2.first) != typeid(p3.first) || typeid(p2.second) != typeid(p3.second))
+		return false;
+	else if (p2.first != p3.first || p2.second != p3.second)
+		return false;
+
+	return true;
+}
+
+inline static bool	__pair_relational_operators( void )
+{
+	std::pair<int,char>	std_p0(nb,c);
+	std::pair<int,char>	std_p1(nb,c);
+	ft::pair<int,char>	ft_p0(nb,c);
+	ft::pair<int,char>	ft_p1(nb,c);
+
+	if (std::operator==(std_p0, std_p1) != ft::operator==(ft_p0, ft_p1) ||
+		std::operator!=(std_p0, std_p1) != ft::operator!=(ft_p0, ft_p1) ||
+		std::operator<(std_p0, std_p1) != ft::operator<(ft_p0, ft_p1) ||
+		std::operator<=(std_p0, std_p1) != ft::operator<=(ft_p0, ft_p1) ||
+		std::operator>(std_p0, std_p1) != ft::operator>(ft_p0, ft_p1) ||
+		std::operator>=(std_p0, std_p1) != ft::operator>=(ft_p0, ft_p1))
+		return false;
+
+	std_p0.first += 1;
+	std_p0.second += 1;
+	ft_p0.first += 1;
+	ft_p0.second += 1;
+
+	if (std::operator==(std_p0, std_p1) != ft::operator==(ft_p0, ft_p1) ||
+		std::operator!=(std_p0, std_p1) != ft::operator!=(ft_p0, ft_p1) ||
+		std::operator<(std_p0, std_p1) != ft::operator<(ft_p0, ft_p1) ||
+		std::operator<=(std_p0, std_p1) != ft::operator<=(ft_p0, ft_p1) ||
+		std::operator>(std_p0, std_p1) != ft::operator>(ft_p0, ft_p1) ||
+		std::operator>=(std_p0, std_p1) != ft::operator>=(ft_p0, ft_p1))
+		return false;
+	
+	std_p1.first += 3;
+	std_p1.second += 3;
+	ft_p1.first += 3;
+	ft_p1.second += 3;
+
+	if (std::operator==(std_p0, std_p1) != ft::operator==(ft_p0, ft_p1) ||
+		std::operator!=(std_p0, std_p1) != ft::operator!=(ft_p0, ft_p1) ||
+		std::operator<(std_p0, std_p1) != ft::operator<(ft_p0, ft_p1) ||
+		std::operator<=(std_p0, std_p1) != ft::operator<=(ft_p0, ft_p1) ||
+		std::operator>(std_p0, std_p1) != ft::operator>(ft_p0, ft_p1) ||
+		std::operator>=(std_p0, std_p1) != ft::operator>=(ft_p0, ft_p1))
+		return false;
+	
+	return true;
+}
+
 void	test_utility( void )
 {
 	t_sub_test	arr[] = {
 		{__pair_default_constructor, "pair default constructor"},
 		{__pair_initialization_constructor, "pair init constructor"},
 		{__pair_copy_constructor, "pair copy constructor"},
+		{__pair_assignment_operator, "pair operator="},
+		{__pair_relational_operators, "pair operators ==, !=, <, <=, >, >="},
 		{NULL, ""}
 	};
 
 	run_sub_tests(arr);
 }
-
