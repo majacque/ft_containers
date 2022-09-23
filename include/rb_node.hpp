67a46568293400436b385e62caf0aee17669f926
Matthieu JACQUET <majacque@e1r10p3.clusters.42paris.fr>
2022-09-23 19:22:57 +0200

    [MAP]   [ADD]   rb_node structure

---
+++ include/rb_node.hpp
@@ -0,0 +1,25 @@
+#ifndef RB_NODE_HPP
+# define RB_NODE_HPP
+
+#include "rb_node_defs.hpp"
+
+namespace ft {
+
+template <typename T>
+struct rb_node
+{
+	typedef T	value_type;
+
+	value_type	val;
+	bool		color;
+	rb_node*	parent;
+	rb_node*	childs[2];
+
+	rb_node( value_type const & val = value_type() ): val(val), color(RED), parent(), childs()
+	{
+		return;
+	}
+};
+}
+
+#endif
---
+++ private/rb_node_defs.hpp
@@ -0,0 +1,10 @@
+#ifndef RB_NODE_DEFS_HPP
+# define RB_NODE_DEFS_HPP
+
+#define	RED	true
+#define BLACK false
+
+#define LEFT 0
+#define RIGHT 1
+
+#endif
